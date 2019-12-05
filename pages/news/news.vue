d810d8b758032bb28fcce5d79868be7031733578
jiumogaoao <394127821@qq.com>
2019-12-06 07:30:46 +0800

    1
    1

--- pages/news/news.vue
+++ pages/news/news.vue
@@ -51,7 +51,7 @@
 			<view class="list">
 				<view class="bigTitle">潮流趋势<view class="line"></view></view>
 				<view class="frame">
-					<view class="point2">
+					<view class="point2" style="background-color: url(/static/BFx00b0ptJs4P2Tw0b1OBB4vWYQqBp.jpg);">
 						<image src="/static/BFx00b0ptJs4P2Tw0b1OBB4vWYQqBp.jpg"></image>
 						<view class="title">维多利亚的秘密</view>
 					</view>
@@ -90,26 +90,23 @@
 				</view>
 			</view>
 			 <view class="list">
-			 	<view class="bigTitle">美容贴士<view class="line"></view></view>
+			 	<view class="bigTitle">健康&美容<view class="line"></view></view>
 			 	<view class="frame">
 			 		<view class="pointS2">
 						
-			 			 <video id="myVideo" src="https://dcloud-img.oss-cn-hangzhou.aliyuncs.com/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20181126.mp4"
+			 			 <video class="video" src="https://dcloud-img.oss-cn-hangzhou.aliyuncs.com/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20181126.mp4"
 			 			                     enable-danmu danmu-btn controls></video>
 			 			<view class="title">维多利亚的秘密</view>
-			 			<view class="button">了解更多</view>
 			 		</view>
 			 		<view class="pointS2">
-			 			<video id="myVideo" src="https://dcloud-img.oss-cn-hangzhou.aliyuncs.com/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20181126.mp4"
+			 			<video class="video" src="https://dcloud-img.oss-cn-hangzhou.aliyuncs.com/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20181126.mp4"
 			 			                    enable-danmu danmu-btn controls></video>
 			 			<view class="title">维多利亚的秘密</view>
-			 			<view class="button">了解更多</view>
 			 		</view>
 			 		<view class="pointS2">
-			 			<video id="myVideo" src="https://dcloud-img.oss-cn-hangzhou.aliyuncs.com/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20181126.mp4"
+			 			<video class="video" src="https://dcloud-img.oss-cn-hangzhou.aliyuncs.com/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20181126.mp4"
 			 			                    enable-danmu danmu-btn controls></video>
 			 			<view class="title">维多利亚的秘密</view>
-			 			<view class="button">了解更多</view>
 			 		</view>
 			 	</view>
 			 </view>
@@ -118,9 +115,82 @@
 		</block>
 		<!-- #endif -->
 		<block  v-if="!destop && initReady">
-			<view class="head">
-				<image class="back" :src="imgPath+'back.png'" @click="back"></image>
-				新闻中心
+			<navBarCP>
+				<view class="nuskinIcon navBarBack" slot="left" @click="back">&#xe63d;</view>
+				<text slot="center">新闻中心</text>
+			</navBarCP>
+			<view style="width:100%;height: 55px;"></view>
+			<swiper class="banner" :indicator-dots="true" :autoplay="true" :interval="1000" :duration="500">
+			                        <swiper-item>
+			                            <image src="/static/20190506FreeShippingPCbanner.png"></image>
+			                        </swiper-item>
+									<swiper-item>
+									    <image src="/static/20190506FreeShippingPCbanner.png"></image>
+									</swiper-item>
+									<swiper-item>
+									    <image src="/static/20190506FreeShippingPCbanner.png"></image>
+									</swiper-item>
+									<swiper-item>
+									    <image src="/static/20190506FreeShippingPCbanner.png"></image>
+									</swiper-item>
+									<swiper-item>
+									    <image src="/static/20190506FreeShippingPCbanner.png"></image>
+									</swiper-item>
+			                    </swiper>
+			<view class="list">
+				<view class="bigTitle">精选文章<view class="line"></view></view>
+				<view class="frame">
+					<view class="point">
+						<image src="/static/BFx00b0ptJs4P2Tw0b1OBB4vWYQqBp.jpg"></image>
+						<view class="title">维多利亚的秘密</view>
+						<view class="text">维多利亚的秘密维多利亚的秘密维多利亚的秘密</view>
+					</view>
+					<view class="point">
+						<image src="/static/BFx00b0ptJs4P2Tw0b1OBB4vWYQqBp.jpg"></image>
+						<view class="title">维多利亚的秘密</view>
+						<view class="text">维多利亚的秘密维多利亚的秘密维多利亚的秘密</view>
+					</view>
+				</view>
+			</view>
+			<view class="list">
+				<view class="frame">
+					<view class="point2" style="background-color: url(/static/BFx00b0ptJs4P2Tw0b1OBB4vWYQqBp.jpg);">
+						<image src="/static/BFx00b0ptJs4P2Tw0b1OBB4vWYQqBp.jpg"></image>
+						<view class="title">维多利亚的秘密</view>
+					</view>
+					<view class="point2">
+						<image src="/static/BFx00b0ptJs4P2Tw0b1OBB4vWYQqBp.jpg"></image>
+						<view class="title">维多利亚的秘密</view>
+					</view>
+					<view class="point2">
+						<image src="/static/BFx00b0ptJs4P2Tw0b1OBB4vWYQqBp.jpg"></image>
+						<view class="title">维多利亚的秘密</view>
+					</view>
+					<view class="point2">
+						<image src="/static/BFx00b0ptJs4P2Tw0b1OBB4vWYQqBp.jpg"></image>
+						<view class="title">维多利亚的秘密</view>
+					</view>
+				</view>
+			</view>
+			<view class="list">
+				<view class="bigTitle">美容贴士<view class="line"></view></view>
+				<swiper class="frame" :indicator-dots="true" :autoplay="false" :interval="1000" :duration="500">
+				                        <swiper-item>
+				                            <image src="/static/20190506FreeShippingPCbanner.png"></image>
+				                        </swiper-item>
+										<swiper-item>
+										    <image src="/static/20190506FreeShippingPCbanner.png"></image>
+										</swiper-item>
+										<swiper-item>
+										    <image src="/static/20190506FreeShippingPCbanner.png"></image>
+										</swiper-item>
+										<swiper-item>
+										    <image src="/static/20190506FreeShippingPCbanner.png"></image>
+										</swiper-item>
+										<swiper-item>
+										    <image src="/static/20190506FreeShippingPCbanner.png"></image>
+										</swiper-item>
+				                    </swiper>
 			</view>
 		</block>
 		
@@ -171,31 +241,138 @@
 		.list{
 			margin-top: 90px;
 			.bigTitle{
+				width:140px;
+				height: 70px;
 				font-size: 35px;
 				text-align: center;
 				font-weight: bold;
 				position: relative;
+				margin: auto;
+				.line{
+					width:40px;
+					height:8px;
+					background-color: $main-hl;
+					margin: auto;
+					position: absolute;
+					bottom:0px;
+					left: 50px;
+				}
+			}
+			.frame{
+				width: 1200px;
+				margin: auto;
+				margin-top: 70px;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				flex-wrap: wrap;
+				.point{
+					margin-top: 58px;
+					width:580px;
+					border: 1px solid $main-gray;
+					padding-bottom: 53px;
+					image{
+						width:100%;
+						height: 411px;
+					}
+					.title{
+						font-size: 33px;
+						font-weight: bold;
+						color:$main-black;
+						margin-top: 33px;
+						margin-left: 40px;
+					}
+					.text{
+						font-size: 14px;
+						margin-top: 24px;
+						color: $main-gray-deep;
+						margin-left: 40px;
+					}
+				}
+				.point2{
+					width: 580px;
+					height: 300px;
+					position: relative;
+					margin-top: 40px;
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					image{
+						width: 100%;
+						height: 100%;
+						position: absolute;
+						top:0;
+						left: 0;
+					}
+					.title{
+						font-size: 33px;
+						font-weight: bold;
+						padding: 26px 40px;
+						color: #fff;
+						position: relative;
+						z-index: 3;
+						background-color: rgba(0,0,0,0.3);
+					}
+				}
+				.pointS{
+					width: 380px;
+					height: 500px;
+					border: 1px solid $main-gray;
+					image{
+						width:100%;
+						height: 300px;
+					}
+					.title{
+						font-size: 20px;
+						font-weight: bold;
+						color:$main-black;
+						margin-top: 30px;
+						margin-left: 26px;
+					}
+					.button{
+						width:250px;
+						height: 50px;
+						background-color: $main-hl;
+						font-size: 17px;
+						color: #fff;
+						line-height: 50px;
+						text-align: center;
+						margin: auto;
+						margin-top: 70px;
+					}
+				}
+				.pointS2{
+					width: 380px;
+					height: 380px;
+					.video{
+						width: 100%;
+						height: 303px;
+					}
+					.title{
+						font-size: 20px;
+						font-weight: bold;
+						color:$main-black;
+						margin-top: 30px;
+					}
+				}
 			}
 		}
 	}
 	/* #endif */
 	.phone{
-		display: flex;
-		align-items: center;
-		flex-direction: column;
-		width:750rpx;
-		.head{
-			width:100%;
-			text-align: center;
-			    font-size: 40rpx;
-			padding-top: 34rpx;
-			position: relative;
-			.back{
-				width:32rpx;
-				height:32rpx;
-				position:absolute;
-				left:20rpx;
+		background-color: #f1f1f1;
+		.banner{
+			width: 750rpx;
+			height: 500rpx;
+			image{
+				width: 750rpx;
+				height: 500rpx;
 			}
+		}
+		.list{
+			margin-top: 75rpx;
+			width: 750rpx;
+			padding: 0 30rpx;
 		}
 	}
 	
